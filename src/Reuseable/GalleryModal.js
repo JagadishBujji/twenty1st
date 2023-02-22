79f3317c4a364c6d0d255458a3f4588fdefecb19
JagadishBujji <jagadish00198@gmail.com>
2023-02-22 13:30:53 +0530

    modal image added

--- src/Reuseable/GalleryModal.js
+++ src/Reuseable/GalleryModal.js
@@ -3,18 +3,21 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import ModalSwiper from "./ModalSwiper";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 800,
+  width: 900,
+  height: 450,
   bgcolor: "background.paper",
-  border: "2px solid #fff",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
-  borderRadius: "10px"
+  borderRadius: "10px",
+  borderRadius: "30px"
 };
 
 export default function GalleryModal(props) {
@@ -38,12 +41,16 @@ export default function GalleryModal(props) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h4" component="h2">
+          <Typography id="modal-modal-title" sx={{mb:2}} variant="h4" component="h2">
             {props.title}
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <ModalSwiper
+            imagecaro1="../images/sf-1.jpg"
+            imagecaro2="../images/sf-2.jpg"
+            imagecaro3="../images/sf-3.jpg"
+            imagecaro4="../images/sf-4.jpg"
+            imagecaro5="../images/sf-5.jpg"
+          />
         </Box>
       </Modal>
     </div>
---
+++ src/Reuseable/ModalSwiper.js
@@ -0,0 +1,79 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../style.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+export default function ModalSwiper(props) {
+  const [swiperRef, setSwiperRef] = useState(null);
+
+  // let appendNumber = 4;
+  // let prependNumber = 1;
+
+  // const prepend2 = () => {
+  //   swiperRef.prependSlide([
+  //     '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+  //     '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+  //   ]);
+  // };
+
+  // const prepend = () => {
+  //   swiperRef.prependSlide(
+  //     '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+  //   );
+  // };
+
+  // const append = () => {
+  //   swiperRef.appendSlide(
+  //     '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+  //   );
+  // };
+
+  // const append2 = () => {
+  //   swiperRef.appendSlide([
+  //     '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+  //     '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+  //   ]);
+  // };
+
+  return (
+    <>
+      <Swiper
+        onSwiper={setSwiperRef}
+        slidesPerView={3}
+        loop={true}
+        centeredSlides={true}
+        spaceBetween={60}
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="slideswiper">
+          <img src={props.imagecaro1} alt="" className="imagecaro" />
+        </SwiperSlide>
+        <SwiperSlide className="slideswiper">
+          <img src={props.imagecaro2} alt="" className="imagecaro" />
+        </SwiperSlide>
+        <SwiperSlide className="slideswiper">
+          <img src={props.imagecaro3} alt="" className="imagecaro" />
+        </SwiperSlide>
+        <SwiperSlide className="slideswiper">
+          <img src={props.imagecaro4} alt="" className="imagecaro" />
+        </SwiperSlide>
+        <SwiperSlide className="slideswiper">
+          <img src={props.imagecaro5} alt="" className="imagecaro" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
