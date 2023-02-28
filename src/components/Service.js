4796d13389d0a7eeb08b65e52250c48579e4c0b5
sanjay-3129 <ksanjay00197@gmail.com>
2023-02-28 20:03:43 +0530

    scrollmagic enters

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,78 +1,56 @@
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+import React, { useEffect, useState } from "react";
+import { Controller, Scene } from "react-scrollmagic";
+// import gsap from "gsap";
+// import ScrollTrigger from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 const Service = () => {
-  const designRef = useRef();
+  const [sidebarTop, setSidebarTop] = useState(undefined);
+  const [isPin, setIsPin] = useState(true);
 
-  // useEffect(() => {
+  useEffect(() => {
+    const chatEl = document.querySelector(".sidebar").getBoundingClientRect();
+    setSidebarTop(chatEl.top);
+  }, []);
 
-  //   // console.log(designRef.offsetTop, window.pageYOffset);
-  // });
+  useEffect(() => {
+    // setIsPin(true);
+    if (!sidebarTop) return;
 
-  // useEffect(() => {
-  //   function handleScroll() {
-  //     const element = designRef.current;
-  //     // const elementPosition = element.getBoundingClientRect().top;
-  //     // const elDistanceToTop =
-  //     //   window.pageYOffset + element.getBoundingClientRect().top;
+    window.addEventListener("scroll", isSticky);
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  }, [sidebarTop]);
 
-  //     const elementPosition = element.getBoundingClientRect().top;
-  //     const distanceFromTop = window.pageYOffset + elementPosition;
-  //     console.log(distanceFromTop);
-  //     // const threshold = 50; // adjust this value as needed
-  //     // if (distanceFromTop < threshold) {
-  //     //   // trigger your function here
-  //     //   console.log("Element is nearly at the top!");
-  //     // }
-  //   }
+  const isSticky = (e) => {
+    const chatEl = document.querySelector(".sidebar");
+    const scrollTop = window.scrollY;
+    if (scrollTop >= sidebarTop - 10) {
+      // chatEl.classList.add("sticky");
+      // setIsPin(true);
+    } else {
+      // setIsPin(false);
+      // chatEl.classList.remove("sticky");
+    }
+  };
 
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
-
-  // useEffect(() => {
-  //   const element = designRef.current;
-  //   const elementPosition =
-  //     element.getBoundingClientRect().top + window.scrollY;
-  //   console.log("elementPosition: ", elementPosition); // 1093.8000
-
-  //   const handleScroll = () => {
-  //     const scrollPosition = window.scrollY;
-  //     // console.log(elementPosition + 400, scrollPosition);
-  //     // const difference = elementPosition + 400 - scrollPosition;
-  //     // console.log("scroll: ", scrollPosition, difference);
-
-  //     // if (difference < 50) {
-  //     if (parseInt(elementPosition + 400) === scrollPosition) {
-  //       // trigger your function here
-  //       console.log("Element is near the top!");
-  //       element.classList.add("sticky");
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
+  console.log(isPin);
 
   return (
     <>
       <div className="p-5 container">
-        <div className="row m-0 sticky">
-          <div className="col-md-7">
-            <div className="row de" ref={designRef}>
-              <span className="dashed">
-                <span className="circle-desi"></span>
-              </span>{" "}
-              <h1 className="desi">Design</h1>
-              {/* <ul>
+        <Controller>
+          <Scene duration={300} pin={isPin}>
+            <div className="row m-0 sidebar">
+              <div className="col-md-7">
+                <div className="row de">
+                  <span className="dashed">
+                    <span className="circle-desi"></span>
+                  </span>{" "}
+                  <h1 className="desi">Design</h1>
+                  {/* <ul>
                 <li></li>
                 <li></li>
                 <li></li>
@@ -80,18 +58,21 @@ const Service = () => {
                 <li></li>
                 <li></li>
               </ul> */}
+                </div>
+              </div>
+              <div className="col-md-5"></div>
             </div>
-          </div>
-          <div className="col-md-5"></div>
-        </div>
-        <div className="row m-0">
-          <div className="col-md-7">
-            <div className="row de">
-              <span className="dashed">
-                <span className="circle-desi"></span>
-              </span>{" "}
-              <h1 className="desi">Digital & Social</h1>
-              {/* <ul>
+          </Scene>
+
+          <Scene duration={300} pin={isPin}>
+            <div className="row m-0">
+              <div className="col-md-7">
+                <div className="row de">
+                  <span className="dashed">
+                    <span className="circle-desi"></span>
+                  </span>{" "}
+                  <h1 className="desi">Digital & Social</h1>
+                  {/* <ul>
                 <li></li>
                 <li></li>
                 <li></li>
@@ -99,18 +80,21 @@ const Service = () => {
                 <li></li>
                 <li></li>
               </ul> */}
+                </div>
+              </div>
+              <div className="col-md-5"></div>
             </div>
-          </div>
-          <div className="col-md-5"></div>
-        </div>
-        <div className="row m-0">
-          <div className="col-md-7">
-            <div className="row de">
-              <span className="dashed">
-                <span className="circle-desi"></span>
-              </span>{" "}
-              <h1 className="desi">Digital & Social</h1>
-              {/* <ul>
+          </Scene>
+
+          <Scene duration={300} pin={isPin}>
+            <div className="row m-0">
+              <div className="col-md-7">
+                <div className="row de">
+                  <span className="dashed">
+                    <span className="circle-desi"></span>
+                  </span>{" "}
+                  <h1 className="desi">Digital & Social</h1>
+                  {/* <ul>
                 <li></li>
                 <li></li>
                 <li></li>
@@ -118,18 +102,21 @@ const Service = () => {
                 <li></li>
                 <li></li>
               </ul> */}
+                </div>
+              </div>
+              <div className="col-md-5"></div>
             </div>
-          </div>
-          <div className="col-md-5"></div>
-        </div>
-        <div className="row m-0">
-          <div className="col-md-7">
-            <div className="row de">
-              <span className="dashed">
-                <span className="circle-desi"></span>
-              </span>{" "}
-              <h1 className="desi">Films & Production</h1>
-              {/* <ul>
+          </Scene>
+
+          <Scene duration={300} pin={isPin}>
+            <div className="row m-0">
+              <div className="col-md-7">
+                <div className="row de">
+                  <span className="dashed">
+                    <span className="circle-desi"></span>
+                  </span>{" "}
+                  <h1 className="desi">Films & Production</h1>
+                  {/* <ul>
                 <li></li>
                 <li></li>
                 <li></li>
@@ -137,10 +124,12 @@ const Service = () => {
                 <li></li>
                 <li></li>
               </ul> */}
+                </div>
+              </div>
+              <div className="col-md-5"></div>
             </div>
-          </div>
-          <div className="col-md-5"></div>
-        </div>
+          </Scene>
+        </Controller>
       </div>
     </>
   );
