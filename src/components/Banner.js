acf139c29ffcefa006920d984a124decd2e906d7
sanjay-3129 <ksanjay00197@gmail.com>
2023-05-29 11:27:49 +0530

    banner responsive

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,50 +1,16 @@
 import React from "react";
+import "./Banner.css";
 
 const Banner = () => {
-  // const lottie = {
-  //   width: "100%",
-  //   height: "70vh",
-  //   "@media only screen and (max-width: 500px)": {
-  //     width: "100%",
-  //     height: "30vh",
-  //   },
-  // };
   return (
     <>
       {/* white */}
-
       <img
         src="https://firebasestorage.googleapis.com/v0/b/twentyone-st.appspot.com/o/body%2Fheader-1.gif?alt=media&token=0c394cee-e1de-4c4f-86c1-ab1b50865c36"
         alt="loader"
         className="img-fluid"
-        style={{ height: "70vh" }}
+        // style={{ height: "70vh" }}
       />
-
-      {/* <lottie-player
-        src="https://lottie.host/7b45dfa0-822b-43e4-814e-a099b460cac3/5D8uaczosa.json"
-        background="transparent"
-        speed="1"
-        style={{
-          width: "100%",
-          height: "70vh",
-        }}
-        loop
-        autoplay
-      ></lottie-player> */}
-
-      {/* black */}
-
-      {/* <lottie-player
-        src="https://lottie.host/31c362f9-ea34-49cf-85c3-0d601b6c235c/x2jQOVSktg.json"
-        background="transparent"
-        speed="1"
-        style={{
-          width: "100%",
-          height: "70vh",
-        }}
-        loop
-        autoplay
-      ></lottie-player> */}
     </>
   );
 };
--- src/components/BannerDark.js
+++ src/components/BannerDark.js
@@ -1,19 +1,11 @@
 import React from "react";
+import "./Banner.css";
 
 const BannerDark = () => {
-  // const lottie = {
-  //   width: "100%",
-  //   height: "70vh",
-  //   "@media only screen and (max-width: 500px)": {
-  //     width: "100%",
-  //     height: "30vh",
-  //   },
-  // };
   return (
     <>
       {/* black */}
-
-      <lottie-player
+      {/* <lottie-player
         src="https://lottie.host/31c362f9-ea34-49cf-85c3-0d601b6c235c/x2jQOVSktg.json"
         background="transparent"
         speed="1"
@@ -23,7 +15,13 @@ const BannerDark = () => {
         }}
         loop
         autoplay
-      ></lottie-player>
+      ></lottie-player> */}
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/twentyone-st.appspot.com/o/body%2Fheader-2.gif?alt=media&token=eae739eb-94f7-428a-9e56-ddbe73049ff4"
+        alt="loader"
+        className="img-fluid"
+        // style={{ height: "70vh" }}
+      />
     </>
   );
 };
