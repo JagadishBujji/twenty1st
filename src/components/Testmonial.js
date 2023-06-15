38d5854e6aeb925ab9260fa767f7e79bbe71927e
sanjay-3129 <ksanjay00197@gmail.com>
2023-06-15 12:33:29 +0530

    Testimonials updated

--- src/components/Testmonial.js
+++ src/components/Testmonial.js
@@ -1,169 +1,146 @@
 import React from "react";
-const Testmonial = ({ isDarkMode }) => {
+
+const Testimonial = ({ isDarkMode }) => {
   return (
-    <>
-      <div
-        id="carouselExampleIndicators"
-        class="carousel slide container"
-        data-ride="carousel"
-      >
-        <ol class="carousel-indicators">
-          <li
-            data-target="#carouselExampleIndicators"
-            data-slide-to="0"
-            class="active"
-            id="dot"
-          ></li>
-          <li
-            data-target="#carouselExampleIndicators"
-            id="dot"
-            data-slide-to="1"
-          ></li>
-          <li
-            data-target="#carouselExampleIndicators"
-            id="dot"
-            data-slide-to="2"
-          ></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item  active">
-            <div className="row">
-              <div className="col-md-5 image-square">
-                {isDarkMode ? (
-                  <img
-                    src="images/gsquare_white.png"
-                    alt=""
-                    className="square"
-                  />
-                ) : (
-                  <img
-                    src="images/gsquare(2).png"
-                    alt=""
-                    className="square"
-                  />
-                )}
-              </div>
-              <div className="col-md-7">
-                <div className="row circle">
-                  <span className="cir"></span>
-                  <span className="cir"></span>
-                  <span className="cir"></span>
-                </div>
-                <div className="row star">
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                </div>
-                <h3 className="say">What Clients say</h3>
-                <p className="founder">founder, Head of UX at twentyone’st</p>
-                <p className="founder">
-                  Behavioral science and Neuromarketing expert
-                </p>
-              </div>
+    <div
+      id="carouselExampleIndicators"
+      className="carousel slide container"
+      data-ride="carousel"
+    >
+      <ol className="carousel-indicators">
+        <li
+          data-target="#carouselExampleIndicators"
+          data-slide-to="0"
+          className="active"
+        ></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <div className="row">
+            <div className="col-md-4 image-square">
+              {isDarkMode ? (
+                <img src="images/sf_white.png" alt="" className="square" />
+              ) : (
+                <img src="images/sf.png" alt="" className="square" />
+              )}
             </div>
-          </div>
-          <div class="carousel-item">
-            <div className="row">
-              <div className="col-md-5 image-square">
-                {isDarkMode ? (
-                  <img
-                    src="images/gsquare_white.png"
-                    alt=""
-                    className="square"
-                  />
-                ) : (
-                  <img
-                    src="images/gsquare(2).png"
-                    alt=""
-                    className="square"
-                  />
-                )}
+            <div className="col-md-7">
+              <div className="row circle">
+                <span className="cir"></span>
+                <span className="cir"></span>
+                <span className="cir"></span>
               </div>
-              <div className="col-md-7">
-                <div className="row circle">
-                  <span className="cir"></span>
-                  <span className="cir"></span>
-                  <span className="cir"></span>
-                </div>
-                <div className="row star">
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                </div>
-                <h3 className="say">What Clients say</h3>
-                <p className="founder">founder, Head of UX at twentyone’st</p>
-                <p className="founder">
-                  Behavioral science and Neuromarketing expert
-                </p>
+              <div className="row star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
               </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div className="row">
-              <div className="col-md-5 image-square">
-                {isDarkMode ? (
-                  <img
-                    src="../images/gsquare_white.png"
-                    alt=""
-                    className="square"
-                  />
-                ) : (
-                  <img
-                    src="../images/gsquare(2).png"
-                    alt=""
-                    className="square"
-                  />
-                )}
-              </div>
-              <div className="col-md-7">
-                <div className="row circle">
-                  <span className="cir"></span>
-                  <span className="cir"></span>
-                  <span className="cir"></span>
-                </div>
-                <div className="row star">
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                </div>
-                <h3 className="say">What Clients say</h3>
-                <p className="founder">founder, Head of UX at twentyone’st</p>
-                <p className="founder">
-                  Behavioral science and Neuromarketing expert
-                </p>
-              </div>
+              <h3 className="say">
+                The pan-India branding conceptualized and designed by Twenty
+                Onest helped us connect with our target audience, leading to a
+                ₹30 crore (+75% increase) in our revenue!
+              </h3>
+              <p className="founder">Jennifer Joseph</p>
+              <p className="founder">
+                Deputy Manager - Corporate Communications, Sundaram Finance
+              </p>
             </div>
           </div>
         </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
-        >
-          {/* <span class="carousel-control-prev-icon" ></span> */}
-          <i class="fas fa-chevron-left " aria-hidden="true"></i>
-          {/* <span class="sr-only">Previous</span> */}
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          {/* <span class="carousel-control-next-icon" ></span> */}
-          <i class="fas fa-chevron-right" aria-hidden="true"></i>
-          {/* <span class="sr-only">Next</span> */}
-        </a>
+        <div className="carousel-item">
+          <div className="row">
+            <div className="col-md-4 image-square">
+              {isDarkMode ? (
+                <img src="images/sf_white.png" alt="" className="square" />
+              ) : (
+                <img src="images/sf.png" alt="" className="square" />
+              )}
+            </div>
+            <div className="col-md-7">
+              <div className="row circle">
+                <span className="cir"></span>
+                <span className="cir"></span>
+                <span className="cir"></span>
+              </div>
+              <div className="row star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h3 className="say">
+                I like the ingenuity that these folks bring to their work. They
+                are a young team, they think differently, keep up with the
+                trends, yet ensure that they highlight the essence of the brand.
+                Their turnaround is quick along with nearly flawless copy and
+                design. Their service is good, and they are great at building
+                relationships. I’d recommend them to anyone who wants to use
+                their services.
+              </h3>
+              <p className="founder">Sandhya Kumar</p>
+              <p className="founder">
+                Head - Corporate Communications, Sundaram Finance
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <div className="row">
+            <div className="col-md-4 image-square">
+              {isDarkMode ? (
+                <img src="../images/icc.png" alt="" className="square" />
+              ) : (
+                <img src="../images/icc.png" alt="" className="square" />
+              )}
+            </div>
+            <div className="col-md-7">
+              <div className="row circle">
+                <span className="cir"></span>
+                <span className="cir"></span>
+                <span className="cir"></span>
+              </div>
+              <div className="row star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h3 className="say">
+                Technically sound, professional with tremendous work ethics.
+                Knowledgeable in adapting to any field. A systematic manner of
+                work. Feeling proud to be associated with them. Looking forward
+                to our long journey!
+              </h3>
+              <p className="founder">Santhosh Gopi</p>
+              <p className="founder">ICC-Certified Cricket Coach</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+      <a
+        className="carousel-control-prev"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="prev"
+      >
+        <i className="fas fa-chevron-left" aria-hidden="true"></i>
+      </a>
+      <a
+        className="carousel-control-next"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="next"
+      >
+        <i className="fas fa-chevron-right" aria-hidden="true"></i>
+      </a>
+    </div>
   );
 };
 
-export default Testmonial;
+export default Testimonial;
